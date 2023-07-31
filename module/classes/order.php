b868acd0c246dd7365496f421457d77779c38682
Your Name <you@example.com>
2023-07-31 15:31:25 +0700

    fix order

--- module/classes/order.php
+++ module/classes/order.php
@@ -14,7 +14,7 @@ class order
         $this->db = new Database();
         $this->fm = new Format();
     }
-    
+
     public function get_all_order()
     {
         $query = "SELECT * FROM tbl_order";
@@ -52,13 +52,13 @@ class order
         $result = $this->db->select($query);
         return $result;
     }
-    
+
     public function update_order_id($code, $address, $email, $phone, $name, $status, $id)
     {
         $query = "UPDATE tbl_order SET code='$code', address='$address',email='$email', phone='$phone',name='$name',status='$status' WHERE orderId='$id'";
         $this->db->update($query);
     }
-    public function update_status_id($status,$id)
+    public function update_status_id($status, $id)
     {
         $query = "UPDATE tbl_order SET status='$status' WHERE code='$id'";
         $this->db->update($query);
@@ -69,7 +69,8 @@ class order
         $this->db->update($query);
     }
 
-    public function order_deleted($info) {
+    public function order_deleted($info)
+    {
         $query = "INSERT INTO tbl_order_deleted (info) VALUE('$info')";
         $this->db->insert($query);
     }
@@ -78,6 +79,27 @@ class order
         $query = "SELECT * FROM tbl_order_deleted";
         $result = $this->db->select($query);
         return $result;
+    }
+    public function order_admin()
+    {
+        $query = "SELECT *
+            FROM tbl_cart";
+        $result = $this->db->select($query);
+        return $result;
+    }
+    public function xuly($xuly)
+    {
+        $query = "UPDATE tbl_order
+        SET status = status + 1
+        WHERE orderId = $xuly;";
+        $this->db->update($query);
+        echo '<script type="text/javascript">toastr.success("Cập Nhật Thành Công")</script>';
+    }
+    public function deleteOrder($id)
+    {
+        $query = "DELETE FROM tbl_order WHERE orderId = $id";
+        $this->db->delete($query);
+        echo '<script type="text/javascript">toastr.danger("Xóa Thành Công")</script>';
     }
 }
 
--- views/include/action.php
+++ views/include/action.php
@@ -1,101 +1,64 @@
 
-<?php  
+<?php
 
-if(isset($_POST["status_id"])){
-     if($_POST["action"] == "updateStatus")  
-      {     
-        $status_id = $_POST["status_id"] ;
-        $status_value = $_POST["status_value"];
-        if($status_value == "Đã nhận"){
-          $status_value = 1;
-        }else{
-          $status_value = 0;
-        }
-        $order->update_status_id($status_value, $status_id);
-      }
-      if($_POST["action"] == "deleteOrder")  
-      {     
-          $status_id = $_POST["status_id"] ;
-          $status_value = $_POST["status_value"];
-          if($status_value == "Hủy"){
-            $status_value = 2;
-          }elseif($status_value == "Xóa"){
-               $status_value = 3;
-          }else{
-            $status_value = 0;
-          }
-          $order->update_status_id($status_value, $status_id);
-      } 
-      if($_POST["action"] == "cancelOrder")  
-      {     
-          $status_id = $_POST["status_id"] ;
-          $status_value = $_POST["status_value"];
-          if($status_value == "Hủy"){
-            $status_value = 2;
-          }elseif($status_value == "Xóa"){
-               $status_value = 3;
-          }else{
-            $status_value = 0;
-          }
-          $order->update_status_id($status_value, $status_id);
-      }   
+if (isset($_POST["status_id"])) {
+     if ($_POST["action"] == "updateStatus") {
+          $status_id = $_POST["status_id"];
+          $order->update_status_id(4, $status_id);
+     }
+     if ($_POST["action"] == "deleteOrder") {
+          $status_id = $_POST["status_id"];
+          $order->update_status_id(5, $status_id);
+     }
+     if ($_POST["action"] == "cancelOrder") {
+          $status_id = $_POST["status_id"];
+          $order->update_status_id(3, $status_id);
+     }
 }
- if(isset($_POST["product_id"]))  
- {  
-      if($_POST["action"] == "add")  
-      {     
-        $product_id = $_POST["product_id"] ;
-        $product_name = $_POST["product_name"];
-        $product_price = $_POST["product_price"] ; 
-        $product_quantity = $_POST["product_quantity"];
-        $product_discount = $_POST["product_discount"] ;
-        if($product_discount==0 || ""){
-            $price = $product_price;
-        }else{
-            $price = $product_discount;
-        }
-        $product_image = $_POST["product_image"];
-           if(isset($_SESSION["cart"][$product_id]))  
-           {  
-            $_SESSION['cart'][$product_id]['quantity']++;
-               
-           }  
-           else  
-           {  
-                $item_array = array(  
-                     'id'               =>     $product_id,  
-                     'name'               =>     $product_name,  
-                     'price'               =>     $price,  
-                     'quantity'          =>     $product_quantity,
-                     'image'               =>      $product_image
+if (isset($_POST["product_id"])) {
+     if ($_POST["action"] == "add") {
+          $product_id = $_POST["product_id"];
+          $product_name = $_POST["product_name"];
+          $product_price = $_POST["product_price"];
+          $product_quantity = $_POST["product_quantity"];
+          $product_discount = $_POST["product_discount"];
+          if ($product_discount == 0 || "") {
+               $price = $product_price;
+          } else {
+               $price = $product_discount;
+          }
+          $product_image = $_POST["product_image"];
+          if (isset($_SESSION["cart"][$product_id])) {
+               $_SESSION['cart'][$product_id]['quantity']++;
+          } else {
+               $item_array = array(
+                    'id'               =>     $product_id,
+                    'name'               =>     $product_name,
+                    'price'               =>     $price,
+                    'quantity'          =>     $product_quantity,
+                    'image'               =>      $product_image
 
-                );  
-                $_SESSION["cart"][$product_id] = $item_array;  
-           }  
-      }  
-      if($_POST["action"] == "remove")  
-      {  
-           foreach($_SESSION["cart"] as $keys => $values)  
-           {  
-                if($values["product_id"] == $_POST["product_id"])  
-                {  
-                     unset($_SESSION["cart"][$keys]);  
-                     $message = '<label class="text-success">Product Removed</label>';  
-                }  
-           }  
-      }  
-      if($_POST["action"] == "quantity_change")  
-      {  
-           foreach($_SESSION["cart"] as $keys => $values)  
-           {  
-                if($_SESSION["cart"][$keys]['product_id'] == $_POST["product_id"])  
-                {  
-                     $_SESSION["cart"][$keys]['product_quantity'] = $_POST["quantity"];  
-                }  
-           }  
-      }   
- }  
- 
- ?>
+               );
+               $_SESSION["cart"][$product_id] = $item_array;
+          }
+     }
+     if ($_POST["action"] == "remove") {
+          foreach ($_SESSION["cart"] as $keys => $values) {
+               if ($values["product_id"] == $_POST["product_id"]) {
+                    unset($_SESSION["cart"][$keys]);
+                    $message = '<label class="text-success">Product Removed</label>';
+               }
+          }
+     }
+     if ($_POST["action"] == "quantity_change") {
+          foreach ($_SESSION["cart"] as $keys => $values) {
+               if ($_SESSION["cart"][$keys]['product_id'] == $_POST["product_id"]) {
+                    $_SESSION["cart"][$keys]['product_quantity'] = $_POST["quantity"];
+               }
+          }
+     }
+}
+
+?>
 
  
